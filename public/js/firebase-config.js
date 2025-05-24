0e1b23fac50962824ed88a5c8a21a3432122a0d6
kk1110 <kayalkamal1998@gmail.com>
2025-05-24 23:20:22 +0530

    region

--- public/js/firebase-config.js
+++ public/js/firebase-config.js
@@ -6,8 +6,9 @@ import { getStorage } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-
 const firebaseConfig = {
   apiKey: "AIzaSyCOBQLAgmchL7gvwyMUXP3LhzeCPcqzeeA",
   authDomain: "sanas-ae992.firebaseapp.com",
+  databaseURL: "https://sanas-ae992-default-rtdb.asia-southeast1.firebasedatabase.app", // <-- Correct region!
   projectId: "sanas-ae992",
-  storageBucket: "sanas-ae992.firebasestorage.app",
+  storageBucket: "sanas-ae992.appspot.com",
   messagingSenderId: "331618755804",
   appId: "1:331618755804:web:46f700a781d58249bc4019"
 };
